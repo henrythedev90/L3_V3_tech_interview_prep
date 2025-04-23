e2de400274f094f84f36c0f39773d1ae7094c405
henrythedev90 <henrythedev90@gmail.com>
2025-04-23 01:48:25 -0400

    added the jest packages and was able to do test questions

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,37 @@
 // Reverse a string
 function reverseString(str) {
-
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
 }
-  
+
 // Find max in array
 function findMax(arr) {
-
+  let maxNum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (maxNum < arr[i]) {
+      maxNum = arr[i];
+    }
+  }
+  return maxNum;
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+  let right = word.length - 1;
+  let left = 0;
+  while (left < right) {
+    if (word[left] !== word[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
+
+isPalindrome("racecar");
 
 module.exports = { reverseString, findMax, isPalindrome };
